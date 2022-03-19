d4c18f11ecba3df6800924fd1995e5ffb98c2ca8
mcgear <michael.gearhardt@gmail.com>
2022-03-19 09:15:37 -0600

    Updated working as docs only

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,11 +8,11 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://www.fathym.com',
   baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: './img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
@@ -38,10 +38,10 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Fathym',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Fathym Logo',
+          src: 'https://www.fathym.com/plasmic/new_fathym_com/images/logo.png',
         },
         items: [
           {
